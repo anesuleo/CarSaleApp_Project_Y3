965dbe676c027b64712a311f2b65e1deedc1f987
anesu <g00425238@atu.ie>
2024-10-24 14:05:45 +0100

    Created New Project

---
+++ src/main/java/ie/atu/carsaleapp_project_y3/CarSaleAppProjectY3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.carsaleapp_project_y3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSaleAppProjectY3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSaleAppProjectY3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/carsaleapp_project_y3/CarSaleAppProjectY3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.carsaleapp_project_y3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSaleAppProjectY3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
