cf5aea23bd42aca1bfc855d4fcb298ff07078bbc
Tremainm <147380462+Tremainm@users.noreply.github.com>
2024-10-27 13:41:47 +0000

    Added template service and controller class for "car" table & organised into packages.

---
+++ src/main/java/ie/atu/carsaleapp_project_y3/controller/CarController.java
@@ -0,0 +1,13 @@
+package ie.atu.carsaleapp_project_y3.controller;
+
+import ie.atu.carsaleapp_project_y3.entity.Car;
+import ie.atu.carsaleapp_project_y3.service.CarService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+}
---
+++ src/main/java/ie/atu/carsaleapp_project_y3/service/CarService.java
@@ -0,0 +1,9 @@
+package ie.atu.carsaleapp_project_y3.service;
+
+import ie.atu.carsaleapp_project_y3.entity.Car;
+import ie.atu.carsaleapp_project_y3.repository.CarRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarService {
+}
