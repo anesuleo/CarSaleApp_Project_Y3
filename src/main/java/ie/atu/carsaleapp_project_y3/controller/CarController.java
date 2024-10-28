ecd3860c4a8027b296b7f71bea27e3c83b23185f
anesu <g00425238@atu.ie>
2024-10-28 21:25:26 +0000

    Added code to CarService and the Controller to allow communication with Database

--- src/main/java/ie/atu/carsaleapp_project_y3/controller/CarController.java
+++ src/main/java/ie/atu/carsaleapp_project_y3/controller/CarController.java
@@ -2,12 +2,30 @@ package ie.atu.carsaleapp_project_y3.controller;
 
 import ie.atu.carsaleapp_project_y3.entity.Car;
 import ie.atu.carsaleapp_project_y3.service.CarService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
+    private CarService carService;
+
+    public CarController(CarService myService){
+        this.carService = myService;
+    }
+
+    @PostMapping
+    public Car addCar(@RequestBody Car car) {
+        return carService.addCar(car);
+    }
+
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+
 }
--- src/main/java/ie/atu/carsaleapp_project_y3/service/CarService.java
+++ src/main/java/ie/atu/carsaleapp_project_y3/service/CarService.java
@@ -4,6 +4,25 @@ import ie.atu.carsaleapp_project_y3.entity.Car;
 import ie.atu.carsaleapp_project_y3.repository.CarRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CarService {
+
+    private final CarRepository carRepository;
+
+    public CarService(CarRepository carRepository){
+        this.carRepository = carRepository;
+    }
+
+    public Car addCar(Car car) {
+        return carRepository.save(car);
+    }
+
+    public List<Car> getAllCars(){
+        return carRepository.findAll();
+    }
+
 }
