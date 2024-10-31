725d86b185606dd21ecd38c6fdd99e5f3a7a8c8b
Natalie <118202686+Nokuvimba@users.noreply.github.com>
2024-10-31 17:02:16 +0000

    Updated database with password column in customer table

---
+++ carsale_updated3110.sql
@@ -0,0 +1,170 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: carsale
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `car`
+--
+
+DROP TABLE IF EXISTS `car`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car` (
+  `car_id` int NOT NULL AUTO_INCREMENT,
+  `cost` float NOT NULL,
+  `make` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `year` int NOT NULL,
+  `car_info_info_id` int DEFAULT NULL,
+  PRIMARY KEY (`car_id`),
+  UNIQUE KEY `UKlke93w8d791b99obkffy2glo9` (`car_info_info_id`),
+  CONSTRAINT `FK30tauyj2co19n7ycakrs51e2r` FOREIGN KEY (`car_info_info_id`) REFERENCES `car_info` (`info_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car`
+--
+
+LOCK TABLES `car` WRITE;
+/*!40000 ALTER TABLE `car` DISABLE KEYS */;
+INSERT INTO `car` VALUES (1,2000,'Ford','Focus',2008,NULL),(2,7450,'Ford','Focus',2013,NULL),(3,6000,'Ford','Fiesta',2010,NULL),(4,16000,'Skoda','Octavia',2020,NULL),(5,18000,'Audi','A6',2017,NULL);
+/*!40000 ALTER TABLE `car` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `car_info`
+--
+
+DROP TABLE IF EXISTS `car_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car_info` (
+  `info_id` int NOT NULL AUTO_INCREMENT,
+  `car_id` int NOT NULL,
+  `colour` varchar(255) NOT NULL,
+  `engine_size` float NOT NULL,
+  `horsepower` int NOT NULL,
+  PRIMARY KEY (`info_id`),
+  KEY `FKr319deqe0cbxoxjgty2pt2j89` (`car_id`),
+  CONSTRAINT `FKr319deqe0cbxoxjgty2pt2j89` FOREIGN KEY (`car_id`) REFERENCES `car` (`car_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car_info`
+--
+
+LOCK TABLES `car_info` WRITE;
+/*!40000 ALTER TABLE `car_info` DISABLE KEYS */;
+INSERT INTO `car_info` VALUES (1,1,'Blue',1.6,90),(2,2,'White',1.6,113),(3,3,'Red',1.6,110),(4,4,'Grey',1.9,118),(5,5,'Black',2,150);
+/*!40000 ALTER TABLE `car_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `phone_number` int NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'Jsmith@email.com','John','Smith',861234567,''),(2,'Emurray@email.com','Ethan','Murray',861234765,''),(3,'Jowens@email.com','Johnny','Owens',861234703,''),(4,'Atreacy@email.com','Anne','Treacy',861230274,''),(5,'Ofitzpatrick@email.com','Olive','Fitzpatrick',861292400,'');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `location` (
+  `location_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `county` varchar(255) NOT NULL,
+  `eircode` varchar(255) NOT NULL,
+  PRIMARY KEY (`location_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `location`
+--
+
+LOCK TABLES `location` WRITE;
+/*!40000 ALTER TABLE `location` DISABLE KEYS */;
+INSERT INTO `location` VALUES (1,1,'Galway','Ireland','Galway','H91V0V1'),(2,2,'Tuam','Ireland','Galway','H91V0V2'),(3,3,'Boyle','Ireland','Roscommon','F45V0V2'),(4,4,'Castlerea','Ireland','Roscommon','F45V0V9'),(5,5,'Dublin','Ireland','Dublin','A12V0V9');
+/*!40000 ALTER TABLE `location` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `store` (
+  `store_id` int NOT NULL AUTO_INCREMENT,
+  `car_id` int NOT NULL,
+  `customer_id` int DEFAULT NULL,
+  `store_city` varchar(255) NOT NULL,
+  `store_county` varchar(255) NOT NULL,
+  `store_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`store_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store`
+--
+
+LOCK TABLES `store` WRITE;
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+INSERT INTO `store` VALUES (1,1,1,'Galway','Galway','Shaws Motors'),(2,2,2,'Dublin','Dublin','Egans Motors');
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-31 16:56:30
